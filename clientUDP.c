d7450054c62d303d3c487aea0310bd19b8246f11
fed3Rosi <111018892+fed3Rosi@users.noreply.github.com>
2023-06-19 19:53:17 +0200

    ciao

--- clientUDP.c
+++ clientUDP.c
@@ -1,185 +1,183 @@
-#include <sys/types.h> 
-#include <sys/socket.h> 
 #include <arpa/inet.h>
+#include <errno.h>
 #include <fcntl.h>
-#include <unistd.h> 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
-#include <signal.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#define DIM_MAX 5 
-#define SERV_PORT   5193 
-#define MAXLINE	1024
+#define DIM_MAX 5
+#define SERV_PORT 5193
+#define MAXLINE 1024
 
-int dim_send=5;
+int dim_send = 5;
 
-int sockfd;	// descrittore alla socket creata per comunicare con il server 
-struct    sockaddr_in   servaddr;
+int sockfd;  // descrittore alla socket creata per comunicare con il server
+struct sockaddr_in servaddr;
 
 // gestisce il segnale di alarm (TIMEOUT)
-void sig_handler(int signum){
-// qui devo ridurre il len di quanto mi pare
-	congest();
+void sig_handler(int signum) {
+  // qui devo ridurre il len di quanto mi pare
+  congest();
 }
 
 // implementa il controllo della congestione
-void congest(){
-	if(dim_send>2){
-		dim_send--;
-	}	
+void congest() {
+  if (dim_send > 2) {
+    dim_send--;
+  }
 }
 
 // funzione che implementare la send to server
-void csend(char *pkt){
-bool stay=true;
-char * snd_buff=malloc(MAXLINE);
-char * rcv_buff=malloc(MAXLINE);
-char * ack=malloc(100);
-char seq[100];
-int k=0,i=0;
-while(stay){
-	if(i<dim_send){
-		i++;
-		sprintf(seq,"%d ",k);
-		k++;
-		strcat(snd_buff,seq);
+void command_send(char *pkt) {
+  bool stay = true;
+  char *snd_buff = malloc(MAXLINE);
+  char *rcv_buff = malloc(MAXLINE);
+  char *ack = malloc(100);
+  char seq[100];
+  int k = 0, i = 0;
+  while (stay) {
+    if (i < dim_send) {
+      i++;
+      sprintf(seq, "%d ", k);
+      k++;
+      strcat(snd_buff, seq);
 
-		strcat(snd_buff,pkt);
+      strcat(snd_buff, pkt);
 
-		buff[strlen(buff)+1]='\0';
-		//temp=read_file();
-		//strcat(buff+strlen(buff),temp);
-		printf("ecco il buff che passo al server %s\n",buff);
+      buff[strlen(buff) + 1] = '\0';
+      // temp=read_file();
+      // strcat(buff+strlen(buff),temp);
+      printf("ecco il buff che passo al server %s\n", buff);
 
-// Invia al server il pacchetto di richiesta
+      // Invia al server il pacchetto di richiesta
 
-  		if (sendto(sockfd, snd_buff, sizeof(snd_buff) , MSG_DONTWAIT, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
-    			perror("errore in sendto");
-    			exit(1);
- 			 }
+      if (sendto(sockfd, snd_buff, sizeof(snd_buff), MSG_DONTWAIT,
+                 (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("errore in sendto");
+        exit(1);
+      }
 
-	// per implementare il timeout uso SIGALRM
+      // per implementare il timeout uso SIGALRM
 
-		//alarm(0.5);  // Scheduled alarm after 500ms		
-// Legge dal socket il pacchetto di risposta 
+      // alarm(0.5);  // Scheduled alarm after 500ms
+      // Legge dal socket il pacchetto di risposta
 
- if(recvfrom(sockfd, rcv_buff, MAXLINE, MSG_DONTWAIT, (struct sockaddr *) &servaddr, sizeof(servaddr) < 0)){
-    perror("errore in recvfrom");
-    exit(1);
+      if (recvfrom(sockfd, rcv_buff, MAXLINE, MSG_DONTWAIT,
+                   (struct sockaddr *)&servaddr, sizeof(servaddr) < 0)) {
+        perror("errore in recvfrom");
+        exit(1);
+      }
+      while (rcv_buff[i] != ' ') {
+        i++;
+      }
+
+      strncpy(rcv_seq, rcv_buff, i);
+
+      if (strcmp(seq, rcv_seq)) {
+        alarm(0);
+      }
+      printf("ho ricevuto dal server %s\n", pkt);
+
+      // invoco la funzione congest per gestire l'arrivo di un ack aumento len
+      // congest();
+    }
   }
- while(rcv_buff[i] != ' '){
-	 i++;
- }
-
- strncpy(rcv_seq,rcv_buff,i);
-
- if(strcmp(seq,rcv_seq)){
-	 alarm(0);
- }
- printf("ho ricevuto dal server %s\n",pkt);
-
-// invoco la funzione congest per gestire l'arrivo di un ack aumento len 
-  //congest();
-	}
 }
-}
-
-
-
-
 
 // concateno la stringa e creo il comando get da inviare al server
-void cget(){
+void cget() {
+  char *buff = malloc(MAXLINE);
+  char b[MAXLINE - 6];
+  printf("inserire nome file \n");
 
-	char * buff=malloc(MAXLINE);
-	char b[MAXLINE-6];
-	printf("inserire nome file \n");
-
-	fscanf(stdin,"%s",b);
-	snprintf(buff,MAXLINE,"get %s",b);
-	csend(buff);
-
- }
-
-// creo il comando list 
-void clist(){
-	char * buff=malloc(MAXLINE);
-	buff="list";
-	csend(buff);
-
+  fscanf(stdin, "%s", b);
+  snprintf(buff, MAXLINE, "get %s", b);
+  command_send(buff);
+ //file_receive();  
 }
 
-// creo il comando put 
-void cput(){
-	char * buff=malloc(MAXLINE);
-	char b[MAXLINE-6];
-	printf("inserire nome file \n");
-
-	fscanf(stdin,"%s",b);
-
-	snprintf(buff,MAXLINE,"put %s",b);
-
-	// metto un numero perchè cosi gestisco i casi in cui richiedo solo dal caso in cui devo inviare il file e aprire quindi un file leggerlo ecc 
-	csend(buff);
-
+// creo il comando list
+void clist() {
+  char *buff = malloc(MAXLINE);
+  buff = "list";
+  command_send(buff);
+ //file_receive();
 }
 
-// gestisco la richiesta dell'utente 
-void req(){
-	int a;
-   	while(1){
-	printf("Inserire numero 0=get 1=list 2=put : -1 exit \n");
-	fscanf(stdin,"%d",&a);
-	switch (a){
-		case 0:
-			cget();
-			break;
+// creo il comando put
+void cput() {
+  char *buff = malloc(MAXLINE);
+  char b[MAXLINE - 6];
+  printf("inserire nome file \n");
 
-		case 1:
-			clist();
-			break;	
+  fscanf(stdin, "%s", b);
 
-		case 2:
-			cput();
-			break;
-		case -1:
-			return;
-	}
-	}
+  snprintf(buff, MAXLINE, "put %s", b);
+
+  // metto un numero perchè cosi gestisco i casi in cui richiedo solo dal caso
+  // in cui devo inviare il file e aprire quindi un file leggerlo ecc
+  command_send(buff);
+  //file_send(b);	
 }
 
-int main(int argc, char *argv[ ]) {
-  char  recvline[MAXLINE + 1];
-  if (argc != 2) {  //controlla numero degli argomenti 
+// gestisco la richiesta dell'utente
+void req() {
+  int a;
+  while (1) {
+    printf("Inserire numero 0=get 1=list 2=put : -1 exit \n");
+    fscanf(stdin, "%d", &a);
+    switch (a) {
+      case 0:
+        cget();
+        break;
+
+      case 1:
+        clist();
+        break;
+
+      case 2:
+        cput();
+        break;
+      case -1:
+        return;
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  char recvline[MAXLINE + 1];
+  if (argc != 2) {  // controlla numero degli argomenti
     fprintf(stderr, "utilizzo: <indirizzo IP server>\n");
     exit(1);
   }
-  
-// implemento il controllore del segnale 
-  signal(SIGALRM,sig_handler); // Register signal handler
-			       //
-  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {  //crea il socket 
+
+  // implemento il controllore del segnale
+  signal(SIGALRM, sig_handler);  // Register signal handler
+                                 //
+  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {  // crea il socket
     perror("errore in socket");
     exit(1);
   }
 
-  memset((void *)&servaddr, 0, sizeof(servaddr));      // azzera servaddr 
-						       //
-  servaddr.sin_family = AF_INET;       // assegna il tipo di indirizzo 
-				       //
-  servaddr.sin_port = htons(SERV_PORT);  // assegna la porta del server 
-					 //
-  // assegna l'indirizzo del server prendendolo dalla riga di comando. L'indirizzo è una stringa da convertire in intero secondo network byte order. 
+  memset((void *)&servaddr, 0, sizeof(servaddr));  // azzera servaddr
+                                                   //
+  servaddr.sin_family = AF_INET;         // assegna il tipo di indirizzo
+                                         //
+  servaddr.sin_port = htons(SERV_PORT);  // assegna la porta del server
+                                         //
+  // assegna l'indirizzo del server prendendolo dalla riga di comando.
+  // L'indirizzo è una stringa da convertire in intero secondo network byte
+  // order.
   if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
-                // inet_pton (p=presentation) vale anche per indirizzi IPv6 
+    // inet_pton (p=presentation) vale anche per indirizzi IPv6
     fprintf(stderr, "errore in inet_pton per %s", argv[1]);
     exit(1);
   }
 
-  //invoco la funzione per gestire le richieste dell'utente
+  // invoco la funzione per gestire le richieste dell'utente
   req();
-
- }
-
+}
--- serverUDP.c
+++ serverUDP.c
@@ -1,49 +1,48 @@
-#include <sys/types.h> 
-#include <sys/socket.h> 
 #include <arpa/inet.h>
-#include <unistd.h> 
-#include <stdio.h> 
-#include <stdlib.h>
-#include <string.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <signal.h>
-#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <wait.h>
 
 #define L 4
 #define MAXLINE 4096
 
-// vedere gli indirizzi da usare per recvform sendto 
-int TO=0;
-static int rcvlen=100;
-static int	nchildren;
+// vedere gli indirizzi da usare per recvform sendto
+int TO = 0;
+static int rcvlen = 100;
+static int nchildren;
 static pid_t *pids;
-static struct flock	lock_it, unlock_it;
-static int		lock_fd = -1;
-		/* fcntl() will fail if my_lock_init() not called */
+static struct flock lock_it, unlock_it;
+static int lock_fd = -1;
+/* fcntl() will fail if my_lock_init() not called */
 
 // globali cosi posso usare più funzioni per rcvform e sendto
 //
 struct sockaddr_in addr;
-socklen_t addrlen=sizeof(struct sockaddr_in);
+socklen_t addrlen = sizeof(struct sockaddr_in);
 
 // implementa il controllo di segnali per gestire poi i figli
 typedef void Sigfunc(int);
-Sigfunc* signal(int signum, Sigfunc *handler);
+Sigfunc *signal(int signum, Sigfunc *handler);
 
 // implementa la logica di lock/unlock
-void my_lock_init(char *pathname)
-{
-  char	lock_file[1024];
+void my_lock_init(char *pathname) {
+  char lock_file[1024];
 
   /* must copy caller's string, in case it's a constant */
   strncpy(lock_file, pathname, sizeof(lock_file));
-  if ( (lock_fd = mkstemp(lock_file)) < 0) {
+  if ((lock_fd = mkstemp(lock_file)) < 0) {
     fprintf(stderr, "errore in mkstemp");
     exit(1);
   }
 
-  if (unlink(lock_file) == -1) { 	/* but lock_fd remains open */
+  if (unlink(lock_file) == -1) { /* but lock_fd remains open */
     fprintf(stderr, "errore in unlink per %s", lock_file);
     exit(1);
   }
@@ -58,11 +57,10 @@ void my_lock_init(char *pathname)
   unlock_it.l_len = 0;
 }
 
-void my_lock_wait()
-{
+void my_lock_wait() {
   int rc;
 
-  while ( (rc = fcntl(lock_fd, F_SETLKW, &lock_it)) < 0) {
+  while ((rc = fcntl(lock_fd, F_SETLKW, &lock_it)) < 0) {
     if (errno == EINTR)
       continue;
     else {
@@ -72,157 +70,147 @@ void my_lock_wait()
   }
 }
 
-void my_lock_release()
-{
-    if (fcntl(lock_fd, F_SETLKW, &unlock_it) < 0) {
-      fprintf(stderr, "errore fcntl in my_lock_release");
-      exit(1);
-    }
+void my_lock_release() {
+  if (fcntl(lock_fd, F_SETLKW, &unlock_it) < 0) {
+    fprintf(stderr, "errore fcntl in my_lock_release");
+    exit(1);
+  }
 }
 
 // gestice nello specifico il comando get
-void send_get(){
-
-}
+void send_get() {}
 
 // gestice nello specifico il comando put
-void send_put(){
-
-}
+void send_put() {}
 
 // gestice nello specifico il comando list
-void send_list(){
+void send_list() {}
 
-}
-
-// gestisce il comando che il client richiede 
-void send_control(int sockfd)
-{
-  char *buff=malloc(MAXLINE);
-  char * temp_buff=malloc(L);
-  int lenbuff=0;
-  int i=0,k=0,n=0;
+// gestisce il comando che il client richiede
+void send_control(int sockfd) {
+  char *buff = malloc(MAXLINE);
+  char *temp_buff = malloc(L);
+  int lenbuff = 0;
+  int i = 0, k = 0, n = 0;
   char ack[100];
   char seq[100];
-  temp_buff="ciaociaociao";
-   while(1){
-	//ho capito se leggiamo e poi implementiamo la gestione delle cose su altre funzione allora dobbiamo passare anche il buffer sennò quello che leggo cancello dalla socket vedere anche il client in tale caso !!!!!!!
-	  if ( (recvfrom(sockfd,buff, rcvlen, 0, (struct sockaddr *)&addr, &addrlen)) < 0) {
-		perror("errore in recvfrom");
-		exit(-1);
-	  }
+  temp_buff = "ciaociaociao";
+  while (1) {
+    // ho capito se leggiamo e poi implementiamo la gestione delle cose su altre
+    // funzione allora dobbiamo passare anche il buffer sennò quello che leggo
+    // cancello dalla socket vedere anche il client in tale caso !!!!!!!
+    if ((recvfrom(sockfd, buff, rcvlen, 0, (struct sockaddr *)&addr,
+                  &addrlen)) < 0) {
+      perror("errore in recvfrom");
+      exit(-1);
+    }
 
-	  while(buff[i] != ' '){
-		  i++;
-	  }
+    while (buff[i] != ' ') {
+      i++;
+    }
 
-	  while(buff[i+n] != ' '){
-		  n++;
-	  }
+    while (buff[i + n] != ' ') {
+      n++;
+    }
 
-	  strncpy(ack,buff,i);
-	  ack[i+1]=' ';
-	  ack[i+2]='\0';
-	  strncpy(seq,buff+i,k);
-	  printf("ack ricevuto %s\n",ack);
-	  printf("msg ricevuto %s\n",buff);
-	  printf("seq mio che si aspetta %s\n",seq);
-	  char *str=malloc(MAXLINE);
-	  strcat(str,ack);
-	  strcat(str,temp_buff);
+    strncpy(ack, buff, i);
+    ack[i + 1] = ' ';
+    ack[i + 2] = '\0';
+    strncpy(seq, buff + i, k);
+    printf("ack ricevuto %s\n", ack);
+    printf("msg ricevuto %s\n", buff);
+    printf("seq mio che si aspetta %s\n", seq);
+    char *str = malloc(MAXLINE);
+    strcat(str, ack);
+    strcat(str, temp_buff);
 
-	  printf(" invio il msg %s \n",str);
+    printf(" invio il msg %s \n", str);
 
-	if (sendto(sockfd, str, strlen(str), 0, (struct sockaddr *)&addr, &addrlen) < 0){
-		perror("errore in sendto");
-		exit(-1);
-	}
-
+    if (sendto(sockfd, str, strlen(str), 0, (struct sockaddr *)&addr,
+               &addrlen) < 0) {
+      perror("errore in sendto");
+      exit(-1);
+    }
   }
 
-// 	qui devo implementare la logica di risposta del server 
-// 	faccio un while cosi da individuare l'indice che mi serve per poi ottenere il comando scelto dal client
-/*
-	while(buff[i] != " "){
-		i++;
-	}
-	strncpy(temp_buff,buff,i+1);
+  // 	qui devo implementare la logica di risposta del server
+  // 	faccio un while cosi da individuare l'indice che mi serve per poi
+  // ottenere il comando scelto dal client
+  /*
+          while(buff[i] != " "){
+                  i++;
+          }
+          strncpy(temp_buff,buff,i+1);
 
-	//forse serve 
-	
-	temp_buff [i+1]='\0';
+          //forse serve
 
-	// implemento logica  per gestire i vari casi
- 
-	if (strcmp(temp_buff, "get") == 0) {
-		send_get();
-	} 
-	else if (strcmp(temp_buff, "put") == 0){
-		send_put();
-	}
-	else if(strcmp(temp_buff,"list") == 0){
-		send_list();
-	}
+          temp_buff [i+1]='\0';
+
+          // implemento logica  per gestire i vari casi
+
+          if (strcmp(temp_buff, "get") == 0) {
+                  send_get();
+          }
+          else if (strcmp(temp_buff, "put") == 0){
+                  send_put();
+          }
+          else if(strcmp(temp_buff,"list") == 0){
+                  send_list();
+          }
 
 
-	
 
-*/
+
+  */
 }
 
-void child_main(int i, int listenfd, int addrlen){
+void child_main(int i, int listenfd, int addrlen) {
+  printf("child %ld starting\n", (long)getpid());
 
-	printf("child %ld starting\n", (long) getpid());
-
-    	for ( ; ; ) {
-		my_lock_wait(); /* my_lock_wait() usa fcntl() */
-		my_lock_release();
-		// va bene cosi perchè sennò lavora solo una per volta invece cosi solo uno per volta legge il comando 
-		send_control(listenfd); /* processa la richiesta */
-	}
+  for (;;) {
+    my_lock_wait(); /* my_lock_wait() usa fcntl() */
+    my_lock_release();
+    // va bene cosi perchè sennò lavora solo una per volta invece cosi solo uno
+    // per volta legge il comando
+    send_control(listenfd); /* processa la richiesta */
+  }
 }
 
-// creo i processi figli 
-pid_t child_make(int i, int listenfd, int addrlen){
-	pid_t pid;
-	if((pid=fork()) >0 )
-		return(pid);// processo padre
-			    //
-	child_main(i, listenfd, addrlen);// non ritorna mai
+// creo i processi figli
+pid_t child_make(int i, int listenfd, int addrlen) {
+  pid_t pid;
+  if ((pid = fork()) > 0)
+    return (pid);                    // processo padre
+                                     //
+  child_main(i, listenfd, addrlen);  // non ritorna mai
 }
 
 // blocco i segnali esterni tranne alcuni che decido ctrl
-Sigfunc *signal(int signum, Sigfunc *func)
-{
-  struct sigaction      act, oact;
-        /* la struttura sigaction memorizza informazioni riguardanti la
-        manipolazione del segnale */
+Sigfunc *signal(int signum, Sigfunc *func) {
+  struct sigaction act, oact;
+  /* la struttura sigaction memorizza informazioni riguardanti la
+  manipolazione del segnale */
 
   act.sa_handler = func;
-  sigemptyset(&act.sa_mask);    /* non occorre bloccare nessun altro segnale */
+  sigemptyset(&act.sa_mask); /* non occorre bloccare nessun altro segnale */
   act.sa_flags = 0;
-  if (signum != SIGALRM)
-     act.sa_flags |= SA_RESTART;
-  if (sigaction(signum, &act, &oact) < 0)
-    return(SIG_ERR);
-  return(oact.sa_handler);
+  if (signum != SIGALRM) act.sa_flags |= SA_RESTART;
+  if (sigaction(signum, &act, &oact) < 0) return (SIG_ERR);
+  return (oact.sa_handler);
 }
 
-//funzione che gestisce il timeout  
-void sig_time(int signo){
+// funzione che gestisce il timeout
+void sig_time(int signo) {}
 
-}
+// termina i figli non ho stato di zombie
+void sig_int(int signo) {
+  int i;
+  /* terminate all children */
+  for (i = 0; i < nchildren; i++) kill(pids[i], SIGTERM);
+  while (wait(NULL) > 0)
+    ; /* wait for all children */
 
-// termina i figli non ho stato di zombie 
-void sig_int(int signo)
-{
-  int	i;
-	/* terminate all children */
-  for (i = 0; i < nchildren; i++)
-    kill(pids[i], SIGTERM);
-  while (wait(NULL) > 0) ;	/* wait for all children */
-		
-  if (errno != ECHILD)  {
+  if (errno != ECHILD) {
     fprintf(stderr, "errore in wait");
     exit(1);
   }
@@ -230,27 +218,27 @@ void sig_int(int signo)
 }
 
 int main(int argc, char **argv) {
-	if(argc != 4 ){
-		fprintf(stderr," utilizzo:<variabile P, TO , SERV_Port > \n");
-		exit(1);
-	}
-// variabili richieste dalla traccia
-int p=atoi(argv[1]);
-TO=atoi(argv[2]);
-int SERV_PORT=atoi(argv[3]);
+  if (argc != 4) {
+    fprintf(stderr, " utilizzo:<variabile P, TO , SERV_Port > \n");
+    exit(1);
+  }
+  // variabili richieste dalla traccia
+  int p = atoi(argv[1]);
+  TO = atoi(argv[2]);
+  int SERV_PORT = atoi(argv[3]);
 
-	if(SERV_PORT<255){
-		fprintf(stderr,"inserire num. porta >255");
-		exit(1);
-	}
-// variabili per il prefork	
-int listenfd, i;
-//queste due nemmeno servono
-void sig_int(int);
+  if (SERV_PORT < 255) {
+    fprintf(stderr, "inserire num. porta >255");
+    exit(1);
+  }
+  // variabili per il prefork
+  int listenfd, i;
+  // queste due nemmeno servono
+  void sig_int(int);
 
-// variabili normali
-   nchildren=5; // numeri di figli qui possiamo rendere le cose dinamiche
-		//
+  // variabili normali
+  nchildren = 5;  // numeri di figli qui possiamo rendere le cose dinamiche
+                  //
   if ((listenfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) { /* crea il socket */
     perror("errore in socket");
     exit(1);
@@ -258,49 +246,38 @@ void sig_int(int);
 
   memset((void *)&addr, 0, sizeof(addr));
   addr.sin_family = AF_INET;
-  addr.sin_addr.s_addr = htonl(INADDR_ANY); /* il server accetta pacchetti su una qualunque delle sue interfacce di rete */
+  addr.sin_addr.s_addr =
+      htonl(INADDR_ANY); /* il server accetta pacchetti su una qualunque delle
+                            sue interfacce di rete */
   addr.sin_port = htons(SERV_PORT); /* numero di porta del server */
 
   /* assegna l'indirizzo al socket */
-  if (bind(listenfd, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+  if (bind(listenfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
     perror("errore in bind");
     exit(1);
   }
-// implemento la logica della prefork con file locking
+  // implemento la logica della prefork con file locking
 
-// qui alloco un area di memoria per mantenere i pid dei child
-pids = (pid_t *)calloc(nchildren, sizeof(pid_t));
-  if (pids == NULL) 
-  { 
+  // qui alloco un area di memoria per mantenere i pid dei child
+  pids = (pid_t *)calloc(nchildren, sizeof(pid_t));
+  if (pids == NULL) {
     fprintf(stderr, "errore in calloc");
     exit(1);
   }
 
   my_lock_init(("/tmp/lock.XXXXXX")); /* one lock file for all children */
-  for (i = 0; i < nchildren; i++){
-	  //inserisco nell array pids i pid dei figli che mi ritornano dalla child_make
-    pids[i] = child_make(i, listenfd, addrlen);	/* parent returns */
+  for (i = 0; i < nchildren; i++) {
+    // inserisco nell array pids i pid dei figli che mi ritornano dalla
+    // child_make
+    pids[i] = child_make(i, listenfd, addrlen); /* parent returns */
   }
   if (signal(SIGINT, sig_int) == SIG_ERR) {
     fprintf(stderr, "errore in signal INT ");
     exit(1);
   }
-  if(signal(SIGALRM, sig_time) == SIG_ERR){
-	  fprintf(stderr,"errore in signal TO");
-	  exit(1);
+  if (signal(SIGALRM, sig_time) == SIG_ERR) {
+    fprintf(stderr, "errore in signal TO");
+    exit(1);
   }
-  for ( ; ; )
-    pause();	/* everything done by children */
+  for (;;) pause(); /* everything done by children */
 }
-
-
-
-
-
-
-
-
-
-
-
-
